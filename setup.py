adc28987ac3083a865c3e1d4ba89356e26d4c199
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2023-01-12 16:26:31 +0100

    created base structure of the repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the project repo for the unsupervised domain adaptation project submitted to miccai23',
+    author='Kris Dreher',
+    license='MIT',
+)
