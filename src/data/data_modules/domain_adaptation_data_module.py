7b4a294fd656245edb0dc05f4a717088f6cdea96
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2023-01-18 14:31:19 +0100

    Added datasets

---
+++ src/data/data_modules/domain_adaptation_data_module.py
@@ -0,0 +1,84 @@
+import pytorch_lightning as pl
+from omegaconf import DictConfig, OmegaConf
+import os
+from torch.utils.data import DataLoader
+from src.data.datasets import DomainAdaptationDataset
+from src.utils.collate_function import collate
+from typing import Iterable, Sized
+
+
+class DomainAdaptationDataModule(pl.LightningDataModule):
+    def __init__(self, experiment_config: DictConfig):
+        super().__init__()
+
+        self.exp_config = experiment_config
+        self.data_dir_a = self.exp_config.data.data_dir_a
+        self.data_dir_b = self.exp_config.data.data_dir_b
+
+        self.data_config_a = OmegaConf.load(os.path.join(self.data_dir_a, "data_config.yaml"))
+        self.data_config_b = OmegaConf.load(os.path.join(self.data_dir_b, "data_config.yaml"))
+
+        self.prepare_data()
+
+        self.used_channels = self.exp_config.data.used_channels
+        orig_dims = self.data_config_a.dims
+        if isinstance(self.used_channels, int):
+            self.channels = 1
+            orig_dims[0] = self.channels
+        elif isinstance(self.used_channels, (Iterable, Sized)):
+            self.channels = len(self.used_channels)
+            orig_dims[0] = self.channels
+        else:
+            self.channels = orig_dims[0]
+
+        self.dimensions = orig_dims
+
+        self.batch_size = self.exp_config.batch_size
+        self.training_data, self.validation_data, self.test_data = None, None, None
+
+        self.adjust_experiment_config()
+
+    def prepare_data(self):
+        if not self.check_data_consistency():
+            raise ValueError("The two datasets for the domain adaptation are not consistent in their dimensions!")
+
+    def check_data_consistency(self):
+        dimension_consistency = (self.data_config_a.dims == self.data_config_b.dims)
+
+        return True  # dimension_consistency removed check because of inconsistencies in pl versions
+
+    def setup(self, stage: str = None):
+        self.training_data = DomainAdaptationDataset(root_a=os.path.join(self.data_dir_a, "training/*.np*"),
+                                                     root_b=os.path.join(self.data_dir_b, "training/*.np*"),
+                                                     used_channels=self.used_channels,
+                                                     )
+        self.validation_data = DomainAdaptationDataset(root_a=os.path.join(self.data_dir_a, "validation/*.np*"),
+                                                       root_b=os.path.join(self.data_dir_b, "validation/*.np*"),
+                                                       used_channels=self.used_channels,
+                                                       )
+        self.test_data = DomainAdaptationDataset(root_a=os.path.join(self.data_dir_a, "test/*.np*"),
+                                                 root_b=os.path.join(self.data_dir_b, "test/*.np*"),
+                                                 used_channels=self.used_channels,
+                                                 )
+
+    def train_dataloader(self):
+        return DataLoader(self.training_data, batch_size=self.batch_size,
+                          shuffle=self.exp_config.shuffle, num_workers=self.exp_config.num_workers, pin_memory=True,
+                          drop_last=True,
+                          collate_fn=collate)
+
+    def val_dataloader(self):
+        return DataLoader(self.validation_data, batch_size=self.batch_size,
+                          shuffle=True,
+                          num_workers=self.exp_config.num_workers, pin_memory=True,
+                          drop_last=True,
+                          collate_fn=collate)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_data, batch_size=1,
+                          num_workers=self.exp_config.num_workers, pin_memory=True,
+                          drop_last=True,
+                          collate_fn=collate)
+
+    def adjust_experiment_config(self):
+        self.exp_config.data.dimensions = self.dimensions
---
+++ src/data/datasets/__init__.py
@@ -0,0 +1,2 @@
+from src.data.datasets.domain_adaptation_dataset import DomainAdaptationDataset
+from src.data.datasets.waic_dataset import WAICDataset
