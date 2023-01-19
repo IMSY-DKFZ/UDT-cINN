2dfa2282753a61ef165e6cc751241f5fe25fb567
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2023-01-19 15:05:52 +0100

    Added fully connected discriminator

--- src/models/discriminator.py
+++ src/models/discriminator.py
@@ -74,3 +74,80 @@ class MultiScaleDiscriminator(nn.Module):
             else:
                 assert 0, "Unsupported GAN type: {}".format(self.gan_type)
         return loss
+
+
+class DiscriminatorHSI(nn.Module):
+    # Multi-scale discriminator architecture
+    def __init__(self, config, input_dim):
+        super(DiscriminatorHSI, self).__init__()
+        self.n_layer = config.n_layer
+        self.gan_type = config.gan_type
+
+        self.hidden_dim = config.dim
+        self.input_dim = input_dim
+
+        # activation type
+        if config.activ == "relu":
+            self.activation = nn.ReLU(inplace=True)
+        elif config.activ == "lrelu":
+            self.activation = nn.LeakyReLU(0.2, inplace=True)
+        elif config.activ == "none":
+            self.activation = None
+        else:
+            raise KeyError(f"Please use a supported activation type: {config.activ}")
+
+        # dropout layer
+        if config.dropout:
+            self.dropout = nn.Dropout(config.dropout_p)
+        else:
+            self.dropout = None
+
+        self.model = self.build_model()
+
+    def build_model(self):
+        model_list = list()
+
+        model_list.append(nn.Linear(self.input_dim, self.hidden_dim))
+
+        for _ in range(self.n_layer - 1):
+            if self.dropout is not None:
+                model_list.append(self.dropout)
+            if self.activation is not None:
+                model_list.append(self.activation)
+
+            model_list.append(nn.Linear(self.hidden_dim, self.hidden_dim))
+
+        return nn.Sequential(*model_list)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def calc_dis_loss(self, input_fake, input_real):
+        # calculate the loss to train D
+        out0 = self.forward(input_fake)
+        out1 = self.forward(input_real)
+        loss = 0
+
+        if self.gan_type == "lsgan":
+            loss += torch.mean((out0 - 0)**2) + torch.mean((out1 - 1)**2)
+        elif self.gan_type == "nsgan":
+            all0 = Variable(torch.zeros_like(out0.data).cuda(), requires_grad=False)
+            all1 = Variable(torch.ones_like(out1.data).cuda(), requires_grad=False)
+            loss += torch.mean(F.binary_cross_entropy(torch.sigmoid(out0), all0) +
+                               F.binary_cross_entropy(torch.sigmoid(out1), all1))
+        else:
+            assert 0, "Unsupported GAN type: {}".format(self.gan_type)
+        return loss
+
+    def calc_gen_loss(self, input_fake):
+        # calculate the loss to train G
+        out0 = self.forward(input_fake)
+        loss = 0
+        if self.gan_type == "lsgan":
+            loss += torch.mean((out0 - 1)**2) # LSGAN
+        elif self.gan_type == "nsgan":
+            all1 = Variable(torch.ones_like(out0.data).cuda(), requires_grad=False)
+            loss += torch.mean(F.binary_cross_entropy(torch.sigmoid(out0), all1))
+        else:
+            assert 0, "Unsupported GAN type: {}".format(self.gan_type)
+        return loss
