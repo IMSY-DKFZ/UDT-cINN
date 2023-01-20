99238a0089b8aa5b5138b75e9817fed21b351f34
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2023-01-20 16:40:38 +0100

    first version of HSI datamodule

--- src/data/data_modules/__init__.py
+++ src/data/data_modules/__init__.py
@@ -1,9 +1,12 @@
 from src.data.data_modules.domain_adaptation_data_module import DomainAdaptationDataModule
+from src.data.data_modules.domain_adaptation_data_module_hsi import DomainAdaptationDataModuleHSI
 from src.data.data_modules.waic_data_module import WAICDataModule
 
 
 def get_data_module(experiment_name: str):
     data_module = DomainAdaptationDataModule
+    if "hsi" in experiment_name:
+        data_module = DomainAdaptationDataModuleHSI
     if experiment_name == "waic":
         data_module = WAICDataModule
 
