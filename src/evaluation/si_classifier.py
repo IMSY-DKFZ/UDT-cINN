b7591fca72cc3ce7e1c3bed2136c2173a17dadd1
menjivar <l.menjivar@dkfz-heidelberg.de>
2023-02-09 16:25:04 +0100

    refactors spectral imaging classifier

--- src/evaluation/si_classifier.py
+++ src/evaluation/si_classifier.py
@@ -2,12 +2,14 @@ import click
 import numpy as np
 import pandas as pd
 import torch
+import joblib
 from omegaconf import DictConfig
 from pathlib import Path
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, classification_report
+from tqdm import tqdm
 
-from src.data.data_modules.semantic_module import SemanticDataModule
+from src.data.data_modules.semantic_module import SemanticDataModule, EnableTestData
 from src import settings
 
 here = Path(__file__)
@@ -15,38 +17,72 @@ here = Path(__file__)
 
 def get_label_mapping():
     mapping = settings.mapping
+    ignore_organs = ['gallbladder']
     organ_labels = settings.organ_labels
-    content = {k: i for k, i in mapping.items() if i in organ_labels}
+    content = {k: i for k, i in mapping.items() if i in organ_labels and i not in ignore_organs}
     return content
 
 
-def load_data(target: str):
+def load_data():
     conf = dict(
         batch_size=100,
         shuffle=False,
         num_workers=1,
         normalization='standardize',
-        data=dict(mean_a=None, std_a=None, mean_b=None, std_b=None), # this is handled internally by the loader
+        data=dict(mean_a=None, std_a=None, mean_b=None, std_b=None),  # this is handled internally by the loader
     )
     conf = DictConfig(conf)
-    dm = SemanticDataModule(experiment_config=conf, target=target)
+    dm = SemanticDataModule(experiment_config=conf, target='sampled')
     dm.setup(stage='eval')
     tdl = dm.train_dataloader()
-    data_a = tdl.dataset.data_a
-    data_a = ((data_a / torch.linalg.norm(data_a, ord=2, dim=1).unsqueeze(dim=1)) - tdl.dataset.exp_config.data["mean_a"]) / tdl.dataset.exp_config.data["std_a"]
+    # data_a refers to simulations while data_b refers to real data
+    # load real data from test data set
+    with EnableTestData(dm):
+        test_dl = dm.test_dataloader()
+    tdata_b = test_dl.dataset.data_b
+    tdata_b = ((tdata_b / torch.linalg.norm(tdata_b, ord=2, dim=1).unsqueeze(dim=1)) - test_dl.dataset.exp_config.data["mean_b"]) / test_dl.dataset.exp_config.data["std_b"]
+    tseg_b = test_dl.dataset.seg_data_b
+    n_samples = tdata_b.shape[0]
+    # load synthetic data from test data set
+    tdata_a = test_dl.dataset.data_a
+    tdata_a = ((tdata_a / torch.linalg.norm(tdata_a, ord=2, dim=1).unsqueeze(dim=1)) - test_dl.dataset.exp_config.data["mean_a"]) / test_dl.dataset.exp_config.data["std_a"]
+    tseg_a = test_dl.dataset.seg_data_a
+    index = np.random.choice(np.arange(tdata_a.shape[0]), size=n_samples, replace=False)
+    tdata_a = tdata_a[index]
+    tseg_a = tseg_a[index]
+
+    # load real data from train data set
     data_b = tdl.dataset.data_b
     data_b = ((data_b / torch.linalg.norm(data_b, ord=2, dim=1).unsqueeze(dim=1)) - tdl.dataset.exp_config.data["mean_b"]) / tdl.dataset.exp_config.data["std_b"]
-    seg_a = tdl.dataset.seg_data_a
     seg_b = tdl.dataset.seg_data_b
-    vdl = dm.val_dataloader()
-    vdata_a = vdl.dataset.data_a
-    vdata_a = ((vdata_a / torch.linalg.norm(vdata_a, ord=2, dim=1).unsqueeze(dim=1)) - vdl.dataset.exp_config.data["mean_a"]) / vdl.dataset.exp_config.data["std_a"]
-    vdata_b = vdl.dataset.data_b
-    vdata_b = ((vdata_b / torch.linalg.norm(vdata_b, ord=2, dim=1).unsqueeze(dim=1)) - vdl.dataset.exp_config.data["mean_b"]) / vdl.dataset.exp_config.data["std_b"]
-    vseg_a = vdl.dataset.seg_data_a
-    vseg_b = vdl.dataset.seg_data_b
-    results = dict(train=dict(x_a=data_a, y_a=seg_a, x_b=data_b, y_b=seg_b),
-                   val=dict(x_a=vdata_a, y_a=vseg_a, x_b=vdata_b, y_b=vseg_b))
+    index = np.random.choice(np.arange(data_b.shape[0]), size=n_samples, replace=False)
+    data_b = data_b[index]
+    seg_b = seg_b[index]
+
+    # load synthetic data adapted with INNs
+    file = settings.results_dir / 'inn' / 'inn_output.npz'
+    inn_data = np.load(file, allow_pickle=True)
+    data_c = torch.tensor(inn_data['spectra_ab'])
+    # spectra adapted from synthetic to real should be normalized with the statistics of the real data set
+    data_c = ((data_c / torch.linalg.norm(data_c, ord=2, dim=1).unsqueeze(dim=1)) - tdl.dataset.exp_config.data["mean_b"]) / tdl.dataset.exp_config.data["std_b"]
+    seg_c = torch.tensor(inn_data['seg_a'])
+
+    # load data adapted by integrating filter responses of optical system
+    dm = SemanticDataModule(experiment_config=conf, target='adapted')
+    dm.setup(stage='eval')
+    with EnableTestData(dm):
+        test_dl_adapted = dm.test_dataloader()
+    data_d = test_dl_adapted.dataset.data_a
+    data_d = ((data_d / torch.linalg.norm(data_d, ord=2, dim=1).unsqueeze(dim=1)) - test_dl_adapted.dataset.exp_config.data["mean_a"]) / test_dl_adapted.dataset.exp_config.data["std_a"]
+    seg_d = test_dl_adapted.dataset.seg_data_a
+    index = np.random.choice(np.arange(data_d.shape[0]), size=n_samples, replace=False)
+    data_d = data_d[index]
+    seg_d = seg_d[index]
+
+    # assert seg_b.size() == tseg_a.size() == seg_c.size() == seg_d.size() == tseg_b.size(), 'missmatch in data set sizes'
+    results = dict(train=dict(x_real=data_b, y_real=seg_b, x_sampled=tdata_a, y_sampled=tseg_a,
+                              x_adapted_inn=data_c, y_adapted_inn=seg_c, x_adapted=data_d, y_adapted=seg_d),
+                   test=dict(x_real=tdata_b, y_real=tseg_b))
     return results
 
 
@@ -56,33 +92,44 @@ def get_model(x: np.ndarray, y: np.ndarray, **kwargs):
     return model
 
 
-def eval_classification(targets: list):
-    for target in targets:
-        mapping = get_label_mapping()
-        labels = [int(k) for k in mapping]
-        names = [mapping[str(k)] for k in labels]
-        data = load_data(target=target)
-        model = get_model(data.get('train').get('x_a'), data.get('train').get('y_a'), n_jobs=-1, n_estimators=10)
-        score = model.score(data.get('val').get('x_b'), data.get('val').get('y_b'))
-        print(f'score {target}: {score}')
-        y_pred = model.predict(data.get('val').get('x_b'))
-        report = classification_report(data.get('val').get('y_b'), y_pred, target_names=names, labels=labels, output_dict=True)
+def eval_classification():
+    stages = [
+        'real',  # train model on real train set sub sampled to have same size as test set
+        'sampled',  # train model on synthetic test set generated by sampling wavelengths
+        'adapted_inn',  # train model on synthetic test set adapted to real domain via INNs
+        'adapted'  # train model on synthetic test data set generated by integrating filter responses
+    ]
+    mapping = get_label_mapping()
+    labels = [int(k) for k in mapping]
+    names = [mapping[str(k)] for k in labels]
+    data = load_data()
+    for stage in tqdm(stages, desc="iterating stages"):
+        train_data = data.get('train').get(f'x_{stage}')
+        train_labels = data.get('train').get(f'y_{stage}')
+        model = get_model(train_data, train_labels, n_jobs=-1, n_estimators=10)
+        # compute score on test set of real data
+        test_data = data.get('test').get('x_real')
+        test_labels = data.get('test').get('y_real')
+        score = model.score(test_data, test_labels)
+        print(f'score {stage}: {score}')
+        y_pred = model.predict(test_data)
+        report = classification_report(test_labels, y_pred, target_names=names, labels=labels, output_dict=True)
 
         results = pd.DataFrame(report)
-        results.to_csv(settings.results_dir / 'rf' / f'rf_classifier_report_{target}.csv', index=True)
+        results.to_csv(settings.results_dir / 'rf' / f'rf_classifier_report_{stage}.csv', index=True)
 
-        matrix = confusion_matrix(data.get('val').get('y_b'), y_pred, labels=labels, normalize='pred')
-        np.save(str(settings.results_dir / 'rf' / f'rf_classifier_matrix_{target}.npy'), matrix)
-        np.save(str(settings.results_dir / 'rf' / f'rf_classifier_y_val_{target}.npy'), data.get('val').get('y_b'))
-        np.save(str(settings.results_dir / 'rf' / f'rf_classifier_y_val_pred_{target}.npy'), y_pred)
+        matrix = confusion_matrix(test_labels, y_pred, labels=labels, normalize='pred')
+        np.save(str(settings.results_dir / 'rf' / f'rf_classifier_matrix_{stage}.npy'), matrix)
+        np.save(str(settings.results_dir / 'rf' / f'rf_classifier_y_val_{stage}.npy'), test_labels)
+        np.save(str(settings.results_dir / 'rf' / f'rf_classifier_y_val_pred_{stage}.npy'), y_pred)
+        joblib.dump(model, str(settings.results_dir / 'rf' / f'rf_classifier_{stage}.joblib'))
 
 
 @click.command()
 @click.option('--rf', is_flag=True, help="evaluate random forest classifier")
-@click.option('-t', '--targets', multiple=True, default=['sampled', 'adapted'], help="target datasets, to evaluate")
-def main(rf: bool, targets: list):
+def main(rf: bool):
     if rf:
-        eval_classification(targets=targets)
+        eval_classification()
 
 
 if __name__ == '__main__':
