766da72e3deea9d76a41a6fc53b24318732eacb3
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2023-02-17 18:36:46 +0100

    Updated paths for bash scripts

--- src/run_training_cluster.sh
+++ src/run_training_cluster.sh
@@ -10,6 +10,10 @@ export DATA_BASE_PATH="/dkfz/cluster/gpu/data/OE0176/dreherk/domain_adaptation_d
 
 export SAVE_DATA_PATH="/dkfz/cluster/gpu/checkpoints/OE0176/dreherk/domain_adaptation_results"
 
+export PATH_MICCAI_23_INTERMEDIATES="/dkfz/cluster/gpu/data/OE0176/dreherk/domain_adaptation_data/HSI_Data/organ_data/intermediates"
+export PATH_MICCAI_23_PROJECT="/dkfz/cluster/gpu/data/OE0176/dreherk/domain_adaptation_data/HSI_Data/organ_data"
+export PATH_MICCAI_23_SEMANTIC_DATASET="/dkfz/cluster/gpu/data/OE0176/dreherk/domain_adaptation_data/HSI_Data/organ_data/intermediates/semantic"
+
 export PYTHON_PATH="$PWD"
 
 python3 train.py "${@:2}"
