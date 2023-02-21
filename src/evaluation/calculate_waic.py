c05c7913566538c3f87ddc102a12ef9889aa4a5f
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2023-02-21 18:54:07 +0100

    Added WAIC visualization

--- src/evaluation/calculate_waic.py
+++ src/evaluation/calculate_waic.py
@@ -6,6 +6,8 @@ from pathlib import Path
 from src.trainers import WAICTrainer
 from src.utils.config_io import load_config
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 here = Path(__file__)
 np.random.seed(141)
@@ -13,27 +15,27 @@ np.random.seed(141)
 
 def load_data():
 
-    test_real_data_root = "/home/kris/Work/Data/domain_adaptation_simulations/min_max_preprocessed_data_sqrt_ms/real_images/test"
+    test_real_data_root = "/home/kris/Work/Data/domain_adaptation_simulations/min_max_preprocessed_data_sqrt_ms/real_images/validation"
     sim_root = "/home/kris/Work/Data/domain_adaptation_simulations/min_max_preprocessed_data_sqrt_ms/good_simulations/test"
     gan_cinn_root = "/home/kris/Work/Data/DA_results/gan_cinn/2023_01_23_22_47_44/testing/generated_image_data"
     unit_root = "/home/kris/Work/Data/DA_results/miccai/domain_adaptation_results/unit/2023_01_21_20_14_58/testing/generated_image_data"
 
-    test_real_data = glob.glob(os.path.join(test_real_data_root, "*.npz"))
+    test_real_data = glob.glob(os.path.join(test_real_data_root, "Forearm*.npz"))
     sim_data = glob.glob(os.path.join(sim_root, "*.npz"))
     gan_cinn_data = glob.glob(os.path.join(gan_cinn_root, "*.npz"))
     unit_data = glob.glob(os.path.join(unit_root, "*.npz"))
 
     datasets = {
-        "test_real_spectra": test_real_data,
-        "sim_spectra": sim_data,
-        "gan_cinn_spectra": gan_cinn_data,
-        "unit_spectra": unit_data,
+        "test_real_images": test_real_data,
+        "sim_images": sim_data,
+        "gan_cinn_images": gan_cinn_data,
+        "unit_images": unit_data,
     }
 
     for dataset_name, dataset_file_list in datasets.items():
-        if dataset_name in ["test_real_spectra", "sim_spectra"]:
+        if dataset_name in ["test_real_images", "sim_images"]:
             datasets[dataset_name] = [np.load(file)["reconstruction"] for file in dataset_file_list[:10]]
-        elif dataset_name in ["gan_cinn_spectra", "unit_spectra"]:
+        elif dataset_name in ["gan_cinn_images", "unit_images"]:
             datasets[dataset_name] = [np.load(file)["images_ab"] for file in dataset_file_list[:10]]
 
     return datasets
@@ -72,10 +74,12 @@ def calculate_waic_score():
             model_results = [model.maximum_likelihood_loss(*model(input_tensor)).detach() for model in models.values()]
             model_results = [tensor.cpu().numpy() for tensor in model_results]
 
-            results_dict[dataset_name].append(np.mean(model_results) + np.var(model_results))
+            results_dict[dataset_name].append(np.var(model_results) + np.mean(model_results))
 
-    results = [np.array(dataset_result) for dataset_result in results_dict.values()]
-    plt.violinplot(results)
+    df = pd.melt(pd.DataFrame({key: np.array(value) for key, value in results_dict.items()}),
+                 var_name="Model", value_name="WAIC Score")
+
+    sns.violinplot(data=df, x="Model", y="WAIC Score", hue="Model", palette="viridis", dodge=False)
     plt.show()
 
 
