5c9f03e4c708d273198828f24d15cbf801a6dc70
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2023-02-23 14:56:27 +0100

    Update test script

--- src/test.py
+++ src/test.py
@@ -1,12 +1,13 @@
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
-import matplotlib.pyplot as plt
+from pytorch_lightning.callbacks import ModelCheckpoint
 import os
 from datetime import datetime
 
-from src.trainers import CondinitionalDomainAdaptationINN, GANDomainAdaptationInn, UNIT, GanCondinitionalDomainAdaptationINN
-from src.data import DomainAdaptationDataModule
-from src.utils.config_io import load_config
+from src.trainers import get_model
+from src.data import get_data_module
+from src.utils.config_io import load_config, get_conf_path
+from src.utils.parser import DomainAdaptationParser
 
 
 try:
@@ -16,42 +17,51 @@ except KeyError:
     run_by_bash: bool = False
 
 if run_by_bash:
-    CONFIG_PATH = os.environ['CONFIG_PATH']
+    EXPERIMENT_NAME = os.environ['EXPERIMENT_NAME']
     SAVE_DATA_PATH = os.environ["SAVE_DATA_PATH"]
-    EXPERIMENT_NAME = os.environ["EXPERIMENT_NAME"]
-    DATA_DIR_A = os.environ["DATA_DIR_A"]
-    DATA_DIR_B = os.environ["DATA_DIR_B"]
+    DATA_BASE_PATH = os.environ["DATA_BASE_PATH"]
+    PYTHON_PATH = os.environ["PYTHON_PATH"]
 
 else:
-    # CONFIG_PATH = "/home/kris/Work/Repositories/dreherk/DomainAdaptation/domain_adaptation/configs/unit_conf.yaml"
-    CONFIG_PATH = "/home/kris/Work/Repositories/dreherk/DomainAdaptation/domain_adaptation/configs/unit_mnist_conf.yaml"
-    SAVE_DATA_PATH = "/home/kris/Work/Data/Test"
-    EXPERIMENT_NAME = "Domain_Adaptation_Run"
-    # DATA_DIR_A = "/home/kris/Work/Data/domain_adaptation_simulations/preprocessed_data/DAS"
-    DATA_DIR_A = "/home/kris/Work/Data/MNIST"
-    # DATA_DIR_B = "/home/kris/Work/Data/domain_adaptation_simulations/preprocessed_data/TR"
-    DATA_DIR_B = "/home/kris/Work/Data/USPS"
+    EXPERIMENT_NAME = "gan_cinn_hsi"
+    SAVE_DATA_PATH = "/home/kris/Work/Data/DA_results"
+    DATA_BASE_PATH = "/home/kris/Work/Data/domain_adaptation_simulations"
+    PYTHON_PATH = "/home/kris/Work/Repositories/miccai23/src"
 
-config = load_config(CONFIG_PATH)
+
+config_path = "/home/kris/Work/Data/DA_results/miccai/domain_adaptation_results/gan_cinn_hsi/2023_02_17_18_41_30/version_0/hparams.yaml"
+config = load_config(config_path)
 time_stamp = datetime.now()
 time_stamp = time_stamp.strftime("%Y_%m_%d_%H_%M_%S")
 
 save_path = os.path.join(SAVE_DATA_PATH, EXPERIMENT_NAME)
-config["save_path"] = save_path
-config["data"]["data_dir_a"] = DATA_DIR_A
-config["data"]["data_dir_b"] = DATA_DIR_B
+config["save_path"] = os.path.join(save_path, time_stamp)
+config["data_base_path"] = DATA_BASE_PATH
+
+parser = DomainAdaptationParser(config=config)
+config = parser.get_new_config()
 
 pl.seed_everything(config.seed)
 
-data_module = DomainAdaptationDataModule(config)
-model = UNIT(experiment_config=config)
+data_module = get_data_module(experiment_name=EXPERIMENT_NAME)
+enable_test_data = False
+if isinstance(data_module, tuple):
+    enable_test_data = True
+    test_data_manager = data_module[1]
+    data_module = data_module[0]
+model = get_model(experiment_name=EXPERIMENT_NAME)
+
+data_module = data_module(experiment_config=config)
+model = model(experiment_config=config)
 logger = TensorBoardLogger(save_dir=save_path, name=time_stamp)
 logger.log_hyperparams(config)
 
-trainer = pl.trainer.Trainer(gpus=1, max_epochs=config.epochs, logger=logger, callbacks=[],
+trainer = pl.trainer.Trainer(accelerator='gpu', devices=1, max_epochs=config.epochs, logger=logger,
+                             callbacks=[],
                              num_sanity_val_steps=0, check_val_every_n_epoch=1,
-                             limit_val_batches=1, gradient_clip_val=0.5,
+                             limit_val_batches=1,
+                             gradient_clip_val=0.1, gradient_clip_algorithm="value",
                              deterministic=False)
 trainer.test(model=model,
-             ckpt_path="/home/kris/Work/Data/DA_results/unit_mnist/2022_09_07_13_18_18/version_0/checkpoints/epoch=999-step=131999.ckpt",
+             ckpt_path="/home/kris/Work/Data/DA_results/miccai/domain_adaptation_results/gan_cinn_hsi/2023_02_17_18_41_30/version_0/checkpoints/epoch=699-step=641200.ckpt",
              datamodule=data_module)
