7e68ff9bd11781f84034b9b238bb4ab632ab4207
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2023-08-03 17:26:10 +0200

    Updated and generalized tests

--- test/test_invertibility.py
+++ test/test_invertibility.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import pytorch_lightning as pl
 
 from src.utils.config_io import load_config
-from src.trainers import GanCondinitionalDomainAdaptationINNHSI, GanCondinitionalDomainAdaptationINN, AlignFlowTrainer
+from src.trainers import GanCondinitionalDomainAdaptationINNHSI, GanCondinitionalDomainAdaptationINN
 
 
 here = Path(__file__).parent
@@ -14,24 +14,26 @@ here = Path(__file__).parent
 class InnInvertibilityTest(unittest.TestCase):
 
     def setUp(self) -> None:
-        pl.seed_everything(100)
 
         self.config = load_config(str(here.parent / "src" / "configs" / "test_conf.yaml"))
-
-        self.model_list = [
-            GanCondinitionalDomainAdaptationINN,
-            GanCondinitionalDomainAdaptationINNHSI,
-            AlignFlowTrainer
-        ]
-
-        self.input_shape_list = [
-            [2, 128, 128],
-            100,
-        ]
+        self.powers_of_two = [2**power for power in range(2, 8)]
+        self.models = {
+            "cINN": {
+                "model": GanCondinitionalDomainAdaptationINN,
+                "data_dimensions": 3
+            },
+            "cINN_HSI": {
+                "model": GanCondinitionalDomainAdaptationINNHSI,
+                "data_dimensions": 1
+            }
+        }
 
     def test_model_invertibility(self):
-        for model, input_shape in zip(self.model_list, self.input_shape_list):
-            self.config.data["dimensions"] = input_shape
+        for model_name, model_specs in self.models.items():
+            model = model_specs["model"]
+            input_shape = [torch.randint(low=2, high=len(self.powers_of_two), size=(1,)).item() for dim in range(model_specs["data_dimensions"])]
+            input_shape = [self.powers_of_two[num] for num in input_shape]
+            self.config.data["dimensions"] = input_shape if len(input_shape) > 1 else input_shape[0]
             self.batch_size = self.config.batch_size
 
             conv_inn = model(self.config)
@@ -39,7 +41,7 @@ class InnInvertibilityTest(unittest.TestCase):
             conv_inn = conv_inn.cuda()
 
             rand_shape = [self.batch_size]
-            rand_shape += input_shape if isinstance(input_shape, list) else [input_shape]
+            rand_shape += input_shape
 
             for i in range(10):
                 x_a = torch.randn(rand_shape).cuda()
