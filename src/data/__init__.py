cc61d311be12cced36a99c8ce90f33d6881dc42e
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2023-10-04 16:30:56 +0200

    Deleted waic-related code

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,5 +1,3 @@
 from src.data.datasets import DomainAdaptationDataset
-from src.data.datasets import WAICDataset
 from src.data.data_modules import DomainAdaptationDataModule
-from src.data.data_modules import WAICDataModule
 from src.data.data_modules import get_data_module
